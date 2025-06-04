2fcf24a358e182963b9215e159f7dfff20e83984
Gustavo Nava <ernestomeraz98@gmail.com>
2025-06-04 10:10:04 -0700

    Se realizo eliminazion de seccion 1

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -32,46 +32,6 @@ const Inicio = () => {
       <Separar />
       <Carrusel />
 
-      <div className="Inicio-Seccion_Uno">
-        <div className="facebook">
-          <div className="facebook-container">
-            <div
-              className="fb-page"
-              data-href="https://www.facebook.com/Nayaritas"
-              data-tabs="timeline"
-              data-width=""
-              data-height=""
-              data-small-header="false"
-              data-adapt-container-width="false"
-              data-hide-cover="false"
-              data-show-facepile="true"
-            >
-              <blockquote
-                cite="https://www.facebook.com/Nayaritas"
-                className="fb-xfbml-parse-ignore"
-              >
-                <a href="https://www.facebook.com/Nayaritas">FENINE</a>
-              </blockquote>
-            </div>
-          </div>
-        </div>
-
-        <div className="espaciador"></div>
-
-        <div className="resena">
-            <h1>{t("inicio.titulo-resena")}</h1>
-            <p>
-                {t("inicio.texto-resena")}
-            </p>
-        </div>
-      </div>
-
-      <div className="video-container">
-        <video width="640" height="360" controls className="video">
-          <source src={video} type="video/mp4"/>
-        </video>
-      </div>
-
       <div className="Inicio-Seccion_Dos">
         <Eventos />
       </div>
